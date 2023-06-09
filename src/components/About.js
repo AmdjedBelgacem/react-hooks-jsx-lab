090b0087b5ea415e67a1c68a2f96f69589e03c20
AmdjedBelgacem <amgedbelgacem@gmail.com>
2023-06-09 20:44:11 +0300

    Done

--- src/components/About.js
+++ src/components/About.js
@@ -2,7 +2,13 @@ import React from "react";
 import { image } from "../data/data";
 
 function About() {
-  return <div>About</div>;
+  return (
+  <div id="about">
+    <h2>About Me</h2>
+    <p>With the courage to begin and the discipline to endure, victory becomes a matter of time</p>
+    <img src={image} alt="I made this"/> 
+  </div>
+  );
 }
 
 export default About;
